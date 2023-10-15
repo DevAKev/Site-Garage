9f2f9f30fce53b025290922429e618dfdc95dffb
Kevyn Aiche <kevynpro7700@gmail.com>
2023-10-15 16:49:10 +0200

    CSS avis & presentation, protection XSS attacks

--- templates/contact_form.php
+++ templates/contact_form.php
@@ -6,20 +6,20 @@
             <p class="text-muted">* Ces champs sont obligatoires.</p>
             <div class="form-group">
                 <label for="form_subject">Objet *</label>
-                <input id="form_subject" type="text" name="subject" class="form-control" placeholder="Entrez l'objet de votre message" value="<?php echo isset($_GET['objet']) ? $_GET['objet'] : ''; ?>" required="required" data-error="Veuillez entrer l'objet de votre message.">
+                <input id="form_subject" type="text" name="subject" class="form-control" placeholder="Entrez l'objet de votre message" value="<?= isset($_GET['objet']) ? htmlspecialchars($_GET['objet'], ENT_QUOTES, 'UTF-8') : ''; ?>" required="required" data-error="Veuillez entrer l'objet de votre message.">
                 <div class="help-block with-errors"></div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="nom">Nom *</label>
-                    <input id="form_name" type="text" name="nom" class="form-control" placeholder="Entrez votre nom" required="required" data-error="Votre nom est requis.">
+                    <input id="form_name" type="text" name="nom" class="form-control" placeholder="Entrez votre nom" required="required" data-error="Votre nom est requis." value="<?= isset($_POST['nom']) ? htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8') : ''; ?>">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="prenom">Prénom *</label>
-                    <input id="form_prenom" type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom">
+                    <input id="form_prenom" type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom" value="<?= isset($_POST['prenom']) ? htmlspecialchars($_POST['prenom'], ENT_QUOTES, 'UTF-8') : ''; ?>">
                     <div class="help-block with-errors"></div>
                 </div>
             </div>
@@ -27,7 +27,7 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label for="phone_number">Numéro *</label>
-                <input id="form_number" type="number" name="phone_number" class="form-control" placeholder="Entrez votre numéro" required="required" data-error="Veuillez entrer un numéro de téléphone valide.">
+                <input id="form_number" type="number" name="phone_number" class="form-control" placeholder="Entrez votre numéro" required="required" data-error="Veuillez entrer un numéro de téléphone valide." value="<?= isset($_POST['phone_number']) ? htmlspecialchars($_POST['phone_number'], ENT_QUOTES, 'UTF-8') : ''; ?>">
                 <div class="help-block with-errors"></div>
             </div>
         </div>
@@ -35,14 +35,14 @@
     <div class="col-md-6">
         <div class="form-group">
             <label for="email">Email *</label>
-            <input id="form_email" type="email" name="email" class="form-control" placeholder="Entrez votre email" required="required" data-error="Veuillez entrer une adresse email valide.">
+            <input id="form_email" type="email" name="email" class="form-control" placeholder="Entrez votre email" required="required" data-error="Veuillez entrer une adresse email valide." value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8') : ''; ?>">
             <div class="help-block with-errors"></div>
         </div>
     </div>
     <div class="col-md-12">
         <div class="form-group">
             <label for="message">Message *</label>
-            <textarea id="form_message" name="message" class="form-control" placeholder="Entrez votre message" rows="4" cols="50" required="required" data-error="Veuillez entrer un message."></textarea>
+            <textarea id="form_message" name="message" class="form-control" placeholder="Entrez votre message" rows="4" cols="50" required="required" data-error="Veuillez entrer un message."><?= isset($_POST['message']) ? htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8') : ''; ?></textarea>
             <div class="help-block with-errors"></div>
         </div>
     </div>
--- templates/editCar_form.php
+++ templates/editCar_form.php
@@ -1,39 +1,39 @@
 <h1>Modifier l'annonce</h1>
 <?php foreach ($messages as $message) { ?>
-    <div class="alert alert-success"><?= $message ?></div>
+    <div class="alert alert-success"><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8') ?></div>
 <?php } ?>
 <?php foreach ($errors as $error) { ?>
-    <div class="alert alert-danger"><?= $error ?></div>
+    <div class="alert alert-danger"><?= htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?></div>
 <?php } ?>
 
 <form method="POST">
     <div class="mb-3">
         <label for="marque" class="form-label">Marque : *</label>
-        <input type="text" name="marque" id="marque" class="form-control" value="<?= htmlentities($car['marque']); ?>">
+        <input type="text" name="marque" id="marque" class="form-control" value="<?= htmlspecialchars($car['marque'], ENT_QUOTES, 'UTF-8') ?>">
     </div>
     <div class="mb-3">
         <label for="modele" class="form-label">Modèle : *</label>
-        <input type="text" name="modele" id="modele" class="form-control" value="<?= htmlentities($car['modele']); ?>">
+        <input type="text" name="modele" id="modele" class="form-control" value="<?= htmlspecialchars($car['modele'], ENT_QUOTES, 'UTF-8') ?>">
     </div>
     <div class="mb-3">
         <label for="prix" class="form-label">Prix : *</label>
-        <input type="number" name="prix" id="prix" class="form-control" value="<?= htmlentities($car['prix']); ?>">
+        <input type="number" name="prix" id="prix" class="form-control" value="<?= intval($car['prix']) ?>">
     </div>
     <div class="mb-3">
         <label for="annee_mise_en_circulation" class="form-label">Année de mise en circulation : *</label>
-        <input type="number" name="annee_mise_en_circulation" id="annee_mise_en_circulation" class="form-control" value="<?= htmlentities($car['annee_mise_en_circulation']); ?>">
+        <input type="number" name="annee_mise_en_circulation" id="annee_mise_en_circulation" class="form-control" value="<?= intval($car['annee_mise_en_circulation']) ?>">
     </div>
     <div class="mb-3">
         <label for="kilometrage" class="form-label">Kilométrage : *</label>
-        <input type="number" name="kilometrage" id="kilometrage" class="form-control" value="<?= htmlentities($car['kilometrage']); ?>">
+        <input type="number" name="kilometrage" id="kilometrage" class="form-control" value="<?= intval($car['kilometrage']) ?>">
     </div>
     <div class="mb-3">
         <label for="caracteristiques" class="form-label">Caractéristiques : *</label>
-        <textarea name="caracteristiques" id="caracteristiques" class="form-control"><?= htmlentities($car['caracteristiques']); ?></textarea>
+        <textarea name="caracteristiques" id="caracteristiques" class="form-control"><?= htmlspecialchars($car['caracteristiques'], ENT_QUOTES, 'UTF-8') ?></textarea>
     </div>
     <div class="mb-3">
         <label for="equipements_options" class="form-label">Equipements et options : *</label>
-        <textarea name="equipements_options" id="equipements_options" class="form-control"><?= htmlentities($car['equipements_options']); ?></textarea>
+        <textarea name="equipements_options" id="equipements_options" class="form-control"><?= htmlspecialchars($car['equipements_options'], ENT_QUOTES, 'UTF-8') ?></textarea>
     </div>
     <div class="mb-3">
         <label for="carburant" class="form-label">Carburant : *</label>
